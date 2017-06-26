af19ebd4a2249f46b6fa912f07b87375a3040d1e
straussreed <straussreed@gmail.com>
2017-06-26 17:17:29 -0500

    now updated the sublime.bat file, it is a little more robust, it prompts me for the github dir, then does a git pull, and a git status. Next I should learn how to make the command line stick to the left, and the sublime stick to the right

--- sublime.bat
+++ sublime.bat
@@ -1,1 +1,6 @@
-START "" "C:\Program files\Sublime text 3\sublime_text.exe" & pushd C:\Users\testbuy\Documents\GitHub
+SET /P gitdir=Enter file directory: & 
+START "" "C:\Program files\Sublime text 3\sublime_text.exe" & 
+pushd C:\Users\testbuy\Documents\GitHub\%gitdir% &
+git pull &
+git status &
+
