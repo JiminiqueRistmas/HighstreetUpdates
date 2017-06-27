c5c6d87881f2fc83773bd5d712eac3da0b2f0890
straussreed <straussreed@gmail.com>
2017-06-26 20:53:33 -0500

    changed how the directories / trees displayed. I wasnted to add color to the tree, and maybe that's still possible, but it's not really relevant at this point.

--- sublime.bat
+++ sublime.bat
@@ -1,5 +1,6 @@
 @ECHO off
-tree C:\Users\testbuy\Documents\GitHub &
+dir /O- C:\Users\testbuy\Documents\GitHub &
+tree /F C:\Users\testbuy\Documents\GitHub &
 SET /P gitdir=Enter desired github directory: & 
 START "" "C:\Program files\Sublime text 3\sublime_text.exe" & 
 pushd C:\Users\testbuy\Documents\GitHub\%gitdir% &
