e360065e8d969d3a6bbc038e74ff9a04346ef7ee
straussreed <straussreed@gmail.com>
2017-06-26 21:19:31 -0500

    nothing to add

--- sublime.bat
+++ sublime.bat
@@ -1,6 +1,5 @@
 @ECHO off
 dir /O- C:\Users\testbuy\Documents\GitHub &
-tree /F C:\Users\testbuy\Documents\GitHub &
 SET /P gitdir=Enter desired github directory: & 
 START "" "C:\Program files\Sublime text 3\sublime_text.exe" & 
 pushd C:\Users\testbuy\Documents\GitHub\%gitdir% &
